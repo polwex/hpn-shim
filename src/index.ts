8e587f8396145128cdbe1097046732947cac474c
polwex <polwex@sortug.com>
2025-04-12 04:45:50 +0700

    added call-provider tool

--- src/index.ts
+++ src/index.ts
@@ -17,16 +17,15 @@ const args = yargs(process.argv.slice(2))
 const SERVER_PATH = "/indexer:hpn:sortugdev.os/api/mcp";
 const DEFAULT_URL = "http://localhost";
 const DEFAULT_PORT = 8080;
+const url =
+  args.u && args.p
+    ? `${args.u}:${args.p}`
+    : args.u && !args.p
+      ? `${args.u}`
+      : !args.u && args.p
+        ? `${DEFAULT_URL}:${args.p}`
+        : `${DEFAULT_URL}:${DEFAULT_PORT}`;
 server.tool("search-registry", { query: z.string() }, async ({ query }) => {
-  const url =
-    args.u && args.p
-      ? `${args.u}:${args.p}`
-      : args.u && !args.p
-        ? `${args.u}`
-        : !args.u && args.p
-          ? `${DEFAULT_URL}:${args.p}`
-          : `${DEFAULT_URL}:${DEFAULT_PORT}`;
-  //
   const body = { SearchRegistry: query };
   try {
     const res = await fetch(url + SERVER_PATH, {
@@ -42,6 +41,34 @@ server.tool("search-registry", { query: z.string() }, async ({ query }) => {
     };
   }
 });
+server.tool(
+  "call-provider",
+  {
+    providerId: z.string(),
+    providerName: z.string(),
+    callArgs: z.record(z.string(), z.any()),
+  },
+  async ({ providerId, providerName, callArgs }) => {
+    const body = {
+      CallProvider: { providerId, providerName, arguments: callArgs },
+    };
+    try {
+      const res = await fetch(url + SERVER_PATH, {
+        method: "POST",
+        headers: { "Content-type": "application/json" },
+        body: JSON.stringify(body),
+      });
+      const resBody = await res.text();
+      return { content: [{ type: "text", text: String(resBody) }] };
+    } catch (e) {
+      return {
+        content: [
+          { type: "text", text: String(`{"error": "Request Failed"}`) },
+        ],
+      };
+    }
+  },
+);
 
 // // Start receiving messages on stdin and sending messages on stdout
 const transport = new StdioServerTransport();
